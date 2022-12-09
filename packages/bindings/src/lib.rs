5b8dc0b04298abfaf713446112cd378a265f73c1
Augusto Elesbão <aelesbao@gmail.com>
2022-12-09 01:02:56 +0100

    feat(bindings): create ArchwayQuery

--- packages/bindings/src/lib.rs
+++ packages/bindings/src/lib.rs
@@ -1,5 +1,9 @@
 mod msg;
+mod pagination;
+mod query;
 
 pub use msg::{ArchwayMsg, WithdrawRewardsResponse};
+pub use pagination::{PageRequest, PageResponse};
+pub use query::{ArchwayQuery, ContractMetadataResponse, RewardsRecord, RewardsRecordsResponse};
 
 pub type Coins = Vec<cosmwasm_std::Coin>;
