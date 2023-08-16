e34126c238a8ef70c49c78cbd908af022df16541
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-08-16 18:34:27 +0200

    feat(proto): impl TypeUrl on all types

--- packages/proto/src/lib.rs
+++ packages/proto/src/lib.rs
@@ -1,3 +1,18 @@
+#![doc = include_str!("../README.md")]
+#![doc(html_logo_url = "https://avatars.githubusercontent.com/u/86496504?s=100")]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![allow(rustdoc::bare_urls, clippy::derive_partial_eq_without_eq)]
+#![forbid(unsafe_code)]
+#![warn(trivial_casts, trivial_numeric_casts, unused_import_braces)]
+
+mod type_urls;
+
+pub use prost;
+pub use prost_types::Any;
+
+pub use cosmos_sdk_proto as cosmos;
+pub use cosmos_sdk_proto::tendermint;
+
 /// The version (commit hash) of the Archway protocol used when generating this library.
 pub const ARCHWAY_VERSION: &str = include_str!("proto/ARCHWAY_COMMIT");
 
