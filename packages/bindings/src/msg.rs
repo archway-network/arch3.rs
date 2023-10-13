69f3a13a2620360131a1e79497d7abad1e306cef
Augusto Elesbão <aelesbao@users.noreply.github.com>
2023-10-13 11:23:00 +0200

    docs(bindings): add documentation

--- packages/bindings/src/msg.rs
+++ packages/bindings/src/msg.rs
@@ -1,19 +1,56 @@
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Coin, CosmosMsg, CustomMsg};
 
+/// Custom messages to interact with the Archway Network bindings.
+///
+/// Those messages work in conjunction with the `cosmwasm_std::CosmosMsg::Custom` variant.
+///
+/// # Examples
+///
+/// ```
+/// use cosmwasm_std::CosmosMsg;
+/// use archway_bindings::ArchwayMsg;
+///
+/// let msg: CosmosMsg<ArchwayMsg> = CosmosMsg::Custom(
+///     ArchwayMsg::UpdateContractMetadata {
+///         contract_address: Some(String::from("contract_address")),
+///         owner_address: Some(String::from("owner")),
+///         rewards_address: Some(String::from("rewards")),
+///     }
+/// );
 #[cw_serde]
 pub enum ArchwayMsg {
+    /// Updates a contract's metadata. Either `owner_address` or `rewards_address` must be provided.
     UpdateContractMetadata {
+        /// If set to `None`, the metadata of the sender contract will be updated.
+        /// In case the `contract_address` already has a metadata, the sender contract must be set
+        /// as the `owner_address` to be able to update it.
         contract_address: Option<String>,
+        /// If set to `None`, the contract's owner will not be updated.
         owner_address: Option<String>,
+        /// If set to `None`, the contract's rewards address will not be updated.
         rewards_address: Option<String>,
     },
+    /// Sets a premium fee for a contract. This action should be executed from a contract only if
+    /// it's set as the `owner_address` in the metadata of `contract_address`. The tx will fail if
+    /// the `contract_address` has no metadata.
     SetFlatFee {
         contract_address: Option<String>,
         flat_fee_amount: Coin,
     },
+    /// Withdraws rewards from the contract. This action should be executed from a contract only if
+    /// it's set as the `rewards_address` in a contract metadata. Only one of `records_limit` or
+    /// `record_ids` should be set.
+    ///
+    /// # See also
+    ///
+    /// * [crate::types::rewards::WithdrawRewardsResponse]
     WithdrawRewards {
+        /// Withdraw rewards up to a specified limit. If set to `None`, all rewards will be
+        /// withdrawn up to the limit of records specified by the governance parameter
+        /// `rewards.MaxWithdrawRecords`.
         records_limit: Option<u64>,
+        /// Withdraw rewards by a list of record IDs.
         record_ids: Vec<u64>,
     },
 }
@@ -27,6 +64,11 @@ impl From<ArchwayMsg> for CosmosMsg<ArchwayMsg> {
 }
 
 impl ArchwayMsg {
+    /// Creates an `ArchwayMsg` to update the current contract's metadata ownership.
+    ///
+    /// # Arguments
+    ///
+    /// * `owner_address` - The new owner address.
     pub fn update_rewards_ownership(owner_address: impl Into<String>) -> Self {
         ArchwayMsg::UpdateContractMetadata {
             contract_address: None,
@@ -35,6 +77,12 @@ impl ArchwayMsg {
         }
     }
 
+    /// Creates an `ArchwayMsg` to update the ownership of an external contract metadata.
+    ///
+    /// # Arguments
+    ///
+    /// * `contract_address` - The other contract address.
+    /// * `owner_address` - The new owner address.
     pub fn update_external_rewards_ownership(
         contract_address: impl Into<String>,
         owner_address: impl Into<String>,
@@ -46,6 +94,11 @@ impl ArchwayMsg {
         }
     }
 
+    /// Creates an `ArchwayMsg` to update the current contract's rewards address.
+    ///
+    /// # Arguments
+    ///
+    /// * `rewards_address` - The new rewards address.
     pub fn update_rewards_address(rewards_address: impl Into<String>) -> Self {
         ArchwayMsg::UpdateContractMetadata {
             contract_address: None,
@@ -54,6 +107,12 @@ impl ArchwayMsg {
         }
     }
 
+    /// Creates an `ArchwayMsg` to update the rewards address of an external contract metadata.
+    ///
+    /// # Arguments
+    ///
+    /// * `contract_address` - The other contract address.
+    /// * `rewards_address` - The new rewards address.
     pub fn update_external_rewards_address(
         contract_address: impl Into<String>,
         rewards_address: impl Into<String>,
@@ -65,6 +124,16 @@ impl ArchwayMsg {
         }
     }
 
+    /// Creates an `ArchwayMsg` to set a flat fee for a contract.
+    ///
+    /// This action should be executed from a contract only if it's set as the `owner_address` in
+    /// the metadata of `contract_address`. The tx will fail if the `contract_address` has no
+    /// metadata.
+    ///
+    /// # Arguments
+    ///
+    /// * `contract_address` - The contract address.
+    /// * `amount` - The flat fee amount.
     pub fn set_flat_fee(contract_address: impl Into<String>, amount: Coin) -> Self {
         ArchwayMsg::SetFlatFee {
             contract_address: Some(contract_address.into()),
@@ -72,6 +141,14 @@ impl ArchwayMsg {
         }
     }
 
+    /// Creates an `ArchwayMsg` to withdraw rewards from the contract.
+    ///
+    /// This action should be executed from a contract only if its set as the `rewards_address` in
+    /// a contract metadata.
+    ///
+    /// # Arguments
+    ///
+    /// * `limit` - Withdraw rewards up to a specified limit.
     pub fn withdraw_rewards_by_limit(limit: u64) -> Self {
         ArchwayMsg::WithdrawRewards {
             records_limit: Some(limit),
@@ -79,6 +156,14 @@ impl ArchwayMsg {
         }
     }
 
+    /// Creates an `ArchwayMsg` to withdraw rewards from the contract by a list of record IDs.
+    ///
+    /// This action should be executed from a contract only if its set as the `rewards_address` in
+    /// a contract metadata.
+    ///
+    /// # Arguments
+    ///
+    /// * `record_ids` - List of record IDs to withdraw rewards from the rewards pool.
     pub fn withdraw_rewards_by_ids(record_ids: Vec<u64>) -> Self {
         ArchwayMsg::WithdrawRewards {
             records_limit: None,
--- packages/bindings/src/pagination.rs
+++ packages/bindings/src/pagination.rs
@@ -1,5 +1,7 @@
 use cosmwasm_schema::cw_serde;
 
+/// WASM binding version for the Cosmos SDK [`query.PageRequest`](https://github.com/cosmos/cosmos-sdk/blob/v0.45.16/proto/cosmos/base/query/v1beta1/pagination.proto),
+/// embedded in request messages for efficient pagination.
 #[cw_serde]
 #[derive(Default)]
 pub struct PageRequest {
@@ -15,34 +17,48 @@ impl PageRequest {
         Self::default()
     }
 
+    /// The `key` is a value returned in `PageResponse.next_key` to begin querying the next page
+    /// most efficiently. Only one of offset or key should be set.
     pub fn key(mut self, key: impl Into<String>) -> Self {
         self.key = Some(key.into());
         self
     }
 
+    /// A numeric offset that can be used when key is unavailable. It is less efficient than using
+    /// key. Only one of offset or key should be set.
     pub fn offset(mut self, offset: u64) -> Self {
         self.offset = Some(offset);
         self
     }
 
+    /// Total number of results to be returned in the result page. If left empty, it will default
+    /// to a value to be set by each app.
     pub fn limit(mut self, limit: u64) -> Self {
         self.limit = Some(limit);
         self
     }
 
+    /// When set to `true`, indicates that the result set should include a count of the total number
+    /// of items available for pagination. `count_total` is only respected when `offset` is used.
+    /// It is ignored when `key` is set.
     pub fn count_total(mut self) -> Self {
         self.count_total = Some(true);
         self
     }
 
+    /// If set to `true`, the results will be returned in the descending order.
     pub fn reverse(mut self) -> Self {
         self.reverse = Some(true);
         self
     }
 }
 
+/// Embedded in response messages where the corresponding request message has used a [`PageRequest`].
 #[cw_serde]
 pub struct PageResponse {
+    /// Key to be passed to `PageRequest.key` to query the next page most efficiently.
     pub next_key: Option<String>,
+    /// Total number of results available if `PageRequest.count_total` was set; its value is
+    /// `None` otherwise
     pub total: Option<u64>,
 }
