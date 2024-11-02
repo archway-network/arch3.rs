b8b5bf702ecd58290a263dda00ba906320c2c19c
Augusto Elesbão <aelesbao@users.noreply.github.com>
2024-11-02 20:32:52 +0100

    style: cargo fmt

--- packages/proto/examples/grpc-client.rs
+++ packages/proto/examples/grpc-client.rs
@@ -6,7 +6,10 @@ use tonic::transport::{Channel, ClientTlsConfig};
 async fn main() -> Result<()> {
     let addr = "https://grpc.mainnet.archway.io:443";
     let tls_config = ClientTlsConfig::new().with_native_roots();
-    let channel = Channel::from_static(addr).tls_config(tls_config)?.connect().await?;
+    let channel = Channel::from_static(addr)
+        .tls_config(tls_config)?
+        .connect()
+        .await?;
 
     let mut client = archway::rewards::v1::query_client::QueryClient::new(channel);
 
